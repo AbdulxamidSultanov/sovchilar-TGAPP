b283c9af4a9884bf76399442ce75a0be9be922f8
itsforme <sultanovabdulxamid@gmail.com>
2025-02-26 23:34:06 +0500

    first commit

--- bot-config/main.py
+++ bot-config/main.py
@@ -1,19 +1,19 @@
-from aiogram import Bot, Dispatcher, types
+from aiogram import Bot, Dispatcher, types, filters
 from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton, WebAppInfo
-from aiogram.filters import Command
+from aiogram.filters import Command  # Новый путь в aiogram 3.x
 import asyncio
 
 TOKEN = "8066095257:AAG9W4w2nyg6WKB7zHt1f-CQ8mtQJPis2wM"
-WEB_APP_URL = "https://sovchilar-tgapp.vercel.app"  # Твоя ссылка
+WEB_APP_URL = "https://sovchilar-tgapp.vercel.app"  # Замени на свою ссылку
 
 bot = Bot(token=TOKEN)
 dp = Dispatcher()
 
 @dp.message(Command("start"))
 async def start_command(message: types.Message):
-    keyboard = InlineKeyboardMarkup()
-    button = InlineKeyboardButton("Открыть веб-приложение", web_app=WebAppInfo(url=WEB_APP_URL))
-    keyboard.add(button)
+    keyboard = InlineKeyboardMarkup(inline_keyboard=[
+        [InlineKeyboardButton(text="Открыть веб-приложение", web_app=WebAppInfo(url=WEB_APP_URL))]
+    ])
 
     await message.answer("Нажмите кнопку ниже, чтобы открыть веб-приложение:", reply_markup=keyboard)
 
--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import { useTelegram } from "@vkruglikov/react-telegram-web-app";
 
 function App() {
+  const { WebApp } = useTelegram();
+
+    useEffect(() => {
+        WebApp.ready();  // Инициализация WebApp API
+        WebApp.expand(); // Разворачивает WebApp на весь экран
+    }, []);
   return (
-    <div className='text-black bg-white min-w-full min-h-[100vh]'>App</div>
+    <div>
+    <h1>Привет, {WebApp.initDataUnsafe?.user?.first_name || "Гость"}!</h1>
+    <button onClick={() => WebApp.close()}>Закрыть</button>
+</div>
   )
 }
 
