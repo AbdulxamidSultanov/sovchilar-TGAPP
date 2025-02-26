ad151461ad8c6b8e69086f322d7744a807fccde7
itsforme <sultanovabdulxamid@gmail.com>
2025-02-26 13:45:48 +0500

    first commit

---
+++ bot-config/index.py
@@ -0,0 +1,20 @@
+from aiogram import Bot, Dispatcher, types
+from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+from aiogram.utils import executor
+
+TOKEN = "8066095257:AAG9W4w2nyg6WKB7zHt1f-CQ8mtQJPis2wM"
+WEB_APP_URL = "https://t.me/sovchilarbot_bot/authorize"  # Ссылка на твою веб-страницу
+
+bot = Bot(token=TOKEN)
+dp = Dispatcher(bot)
+
+@dp.message_handler(commands=["start"])
+async def start_command(message: types.Message):
+    keyboard = InlineKeyboardMarkup()
+    button = InlineKeyboardButton("Открыть веб-приложение", web_app=types.WebAppInfo(url=WEB_APP_URL))
+    keyboard.add(button)
+
+    await message.answer("Нажмите кнопку ниже, чтобы открыть веб-приложение:", reply_markup=keyboard)
+
+if __name__ == "__main__":
+    executor.start_polling(dp)
